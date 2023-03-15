f9b9eea72b559f6f167651a2be17d017c9299efc
Clark Zheng <yiming.zheng@mail.mcgill.ca>
2023-03-15 03:51:22 -0700

    header for http class

--- httpcon.h
+++ httpcon.h
@@ -1,6 +1,21 @@
+#ifndef HTTPCON_H
+#define HTTPCON_H
+
+#include <iostream>
+#include <string>
 #include <curl/curl.h>
 
-
 class HttpCon {
-    
+public:
+    HttpCon(const std::string& url);
+    ~HttpCon();
+    std::string sendGetRequest();
+    std::string sendPostRequest(const std::string& data, const std::string& contentType);
+    std::string sendPutRequest(const std::string& data, const std::string& contentType);
+
+private:
+    CURL* curl;
+    static size_t writeCallback(char* ptr, size_t size, size_t nmemb, std::string* userdata);
 };
+
+#endif
